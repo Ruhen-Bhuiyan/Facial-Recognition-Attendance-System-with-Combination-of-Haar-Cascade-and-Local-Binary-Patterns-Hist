52ebb1c0668959aa31ad130ef2eb861a5ab3e966
Ruhen-Bhuiyan <43878741+Ruhen-Bhuiyan@users.noreply.github.com>
2021-02-24 23:00:54 +0600

    Update abcd.py

--- abcd.py
+++ abcd.py
@@ -12,11 +12,9 @@ seen=[]
 import cv2, sys, numpy, os 
 size = 4
 haar_file = 'haarcascade_frontalface_default.xml'
-datasets = 'datasets'
-  
+datasets = 'datasets'  
 # Part 1: Create fisherRecognizer 
-print('Recognizing Face Please Be in sufficient Lights...') 
-  
+print('Recognizing Face Please Be in sufficient Lights...')  
 # Create a list of images and a list of corresponding names 
 (images, lables, names, id) = ([], [], {}, 0) 
 for (subdirs, dirs, files) in os.walk(datasets): 
@@ -29,11 +27,9 @@ for (subdirs, dirs, files) in os.walk(datasets):
             images.append(cv2.imread(path, 0)) 
             lables.append(int(lable)) 
         id += 1
-(width, height) = (130, 100) 
-  
+(width, height) = (130, 100)   
 # Create a Numpy array from the two lists above 
-(images, lables) = [numpy.array(lis) for lis in [images, lables]] 
-  
+(images, lables) = [numpy.array(lis) for lis in [images, lables]]  
 # OpenCV trains a model from the images 
 # NOTE FOR OpenCV2: remove '.face' 
 model = cv2.face.LBPHFaceRecognizer_create() 
